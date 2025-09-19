e6dfe17f19e597c75eba76319b848e4e51872cf4
jinsol.Baek <emcis44@gmail.com>
2025-09-19 10:22:43 +0900

    250919_10:22

---
+++ rMate/LicenseKey/rMateChartH5License.js
@@ -0,0 +1,11 @@
+// -----------------------------------------------------------------------------
+// rMate Chart H5 License Key 
+//
+// Product Name : rMate Chart for HTML5 v7.0
+// License Type : Enterprise Trial License
+// Product No : 73BA-2336-6758-7EDG
+// Authenticated server Info : undefined
+// Expiration date : 2025/10/31
+//
+var rMateChartH5License = "8febce6ff096e5e8ddaf9e43e2754727b061abab9f7e0b080abd77f3b2de43f7:3300330b3648443a4f4220504c2056473a4432452e37302d2038503556373a36432d35362d3345334e322d2d37412e4230332d37453a564c41204c312030452f4c313a31742f203543323a303232303a32453520302a373a324839";
+// -----------------------------------------------------------------------------
---
+++ rMate/LicenseKey/rMateMapChartH5License.js
@@ -0,0 +1,13 @@
+<!--
+// -----------------------------------------------------------------------------
+// rMate MapChart License Key 
+//
+// Product Name : rMate MapChart for HTML5 v5.5
+// License Type : Enterprise Trial License
+// Product No : CDB3-45B5-D59C-D14N
+// Authenticated Server Info : undefined
+// Expiration Date : 2025/10/31
+//
+var rMateMapChartH5License = "a1356ac4bc453ddcc941a5c666e786c9ed5c1f04ae19f79611683540d20e64f8:3500620b353a484244504f20204e4c3456313a44322d2e43303920355044562d3a354d4235352d34352d2e3335422d444543563a414c4c20203145304c2f3a317431202f43353a3232303032323a35453020372a323a3948";
+// -----------------------------------------------------------------------------
+// -->
