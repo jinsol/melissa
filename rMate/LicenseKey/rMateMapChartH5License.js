4e754191bf45723ed4672c8fac95e4e5dbc9d24b
jinsol.Baek <emcis44@gmail.com>
2026-03-16 14:12:20 +0900

    Add files via upload

--- rMate/LicenseKey/rMateMapChartH5License.js
+++ rMate/LicenseKey/rMateMapChartH5License.js
@@ -1,12 +1,13 @@
+<!--
 // -----------------------------------------------------------------------------
-// rMate MapChart License Key
+// rMate MapChart License Key 
 //
 // Product Name : rMate MapChart for HTML5 v5.5
 // License Type : Enterprise Trial License
-// Product No : 20B9-CC6B-8BAD-C43Y
+// Product No : 7E7B-E1D7-2585-DA5V
 // Authenticated Server Info : undefined
-// Expiration Date : 2026/03/31
+// Expiration Date : 2026/05/51
 //
-var rMateMapChartH5License =
-  "6c80096a2c49b1c457397bd9e8b2e9df829edfd244288038bc57783163c57ed2:6300360b623a484244504f2020594c3356343a43322d2e44304120425038562d3a424d3635432d43352d2e3935422d304532563a414c4c20203145304c2f3a347430202f43363a3232303032323a35453120322a333a3048";
+var rMateMapChartH5License = "0d3a6aa3c75663e910929a50281ddb0385d55669c182bf6563d9b4e38d467cdc:3100640b373a484244504f2020564c3556413a44322d2e35303820355032562d3a374d4435312d45352d2e4235372d454537563a414c4c20203145304c2f3a367430202f43363a3232303032323a36453020322a323a3748";
 // -----------------------------------------------------------------------------
+// -->
